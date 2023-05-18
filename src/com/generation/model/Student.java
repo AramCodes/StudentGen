aa04b43723d872a941a1dfe5c8435bd28ef5558b
Aram Valcourt <115132116+AramCodes@users.noreply.github.com>
2023-05-18 11:50:12 -0400

    correct

---
+++ src/com/generation/model/Student.java
@@ -0,0 +1,63 @@
+package com.generation.model;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+public class Student
+    extends Person
+    implements Evaluation
+{
+    private double average;
+
+    private final List<Course> courses = new ArrayList<>();
+
+    private final Map<String, Course> approvedCourses = new HashMap<>();
+
+    public Student( String id, String name, String email, Date birthDate )
+    {
+        super( id, name, email, birthDate );
+    }
+
+    public void enrollToCourse( Course course )
+    {
+        //TODO
+        if ( !isAttendingCourse(course.getCode() ) ) {
+          courses.add(course);
+       }
+
+    }
+
+    public void registerApprovedCourse( Course course )
+    {
+        approvedCourses.put( course.getCode(), course );
+    }
+
+
+    public boolean isAttendingCourse( String courseCode )
+    {
+        // TODO
+        for (Course course: courses ) {
+            if(course.getCode().equals(courseCode) ){
+                System.out.println("Student is already enrolled in course"+ courseCode+"!");
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public double getAverage()
+    {
+        return average;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Student {" + super.toString() + "}";
+    }
+}
